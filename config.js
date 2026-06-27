6dd6e681455ff6a959822aa68501425a355e8dd9
Bryan Felipe De Souza Santos <137795446+bryanfelipe03@users.noreply.github.com>
2026-06-26 22:19:50 -0300

    Fix comment formatting in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuração do Supabase
 const SUPABASE_URL = 'https://sptmwpiwfhefqchctlag.supabase.co'; // SUBSTITUIR
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwdG13cGl3ZmhlZnFjaGN0bGFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1MDkzMzcsImV4cCI6MjA5ODA4NTMzN30.AWacnxvG6rSBX6ZmfxUWDo4zGwPTvrk3Er3ReBMzDHs'; // SUBSTITUIR
-// Inicializar cliente Supabase no escopo global (window) para evitar
+ //Inicializar cliente Supabase no escopo global (window) para evitar
 // "Identifier 'supabase' has already been declared" entre arquivos
 if (!window._supabaseClient) {
   const { createClient } = window.supabase;
